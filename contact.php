4575b786a910e3caed923b9fac5b8a1b34285cf9
Shkodenko V. Taras <podlom@users.noreply.github.com>
2016-11-02 21:11:57 +0200

    Update contact.php
    Object oriented version of contact form with one ContactForm class

--- contact.php
+++ contact.php
@@ -2,85 +2,142 @@
 /**
  * Created by PhpStorm.
  * User: Тарас
- * Date: 19.10.2016
+ * Date: 02.11.2016
+ *
+ * Object oriented version of contact form with one ContactForm class
+ *
  */
 
-    $commentsFile = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'comments.dat';
-    $formDataMsg = '* - required fields';
-    $uname = $umail = $umsg = '';
-    $formErrors = [];
-    if (!empty($_POST)) {
-        $formDataMsg .= '<br>' . 'Form data: ' . var_export($_POST, 1) . '<br>';
-        if (empty($_POST['uname'])) {
-            $formErrors[] = 'Username can`t be empty!';
-        } else {
-            $uname = $_POST['uname'];
-        }
-        if (empty($_POST['umail'])) {
-            $formErrors[] = 'Email can`t be empty!';
-        } else {
-            $umail = $_POST['umail'];
-        }
-        if (!filter_var($_POST['umail'], FILTER_VALIDATE_EMAIL)) {
-            $formErrors[] = 'Email has wrong format!';
-        }
-        if (empty($_POST['umsg'])) {
-            $formErrors[] = 'Message can`t be empty!';
-        } else {
-            $umsg = $_POST['umsg'];
-        }
-        if (!empty($formErrors)) {
-            $formDataMsg .= 'Can`t add your comment because filled form has validation errors: ' . '<br>' . PHP_EOL;
-            foreach ($formErrors as $err) {
-                $formDataMsg .= $err . '<br>' . PHP_EOL;
+class ContactForm
+{
+    private $commentsFile = '';
+    private $formDataMsg = '* - required fields';
+    private $uname, $umail, $umsg = '';
+    private $formErrors = [];
+
+    public function __construct()
+    {
+        $this->setCommentsFileName();
+        $this->processPostData();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUname()
+    {
+        return $this->uname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUmail()
+    {
+        return $this->umail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUmsg()
+    {
+        return $this->umsg;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormDataMsg()
+    {
+        return $this->formDataMsg;
+    }
+
+    public function displayComments()
+    {
+        $userComments = '';
+        $aComm = $this->getComments();
+        if (!empty($aComm)) {
+            foreach ($aComm as $comm) {
+                if (!empty($comm)) {
+                    $userComments .= '<dl>' .
+                        '<dl>Username: </dl><dd>' . $comm['uname'] . '</dd>' .
+                        '<dl>Email: </dl><dd>' . $comm['umail'] . '</dd>' .
+                        '<dl>Message: </dl><dd>' . $this->filterBadWords($comm['umsg']) . '</dd>' .
+                        '<dl>Added on: </dl><dd>' . $comm['datetime'] . '</dd>' .
+                        '</dl><br>' . PHP_EOL;
+                }
             }
-        } else {
-            $commentData = $_POST;
-            $commentData['datetime'] = date('Y-m-d H:i:s');
-            addComment($commentData);
-            header('Location: ' . $_SERVER['PHP_SELF'] . '?commentAdded' . time());
+        }
+        return $userComments;
+    }
+
+    private function processPostData()
+    {
+        if (!empty($_POST)) {
+            $this->formDataMsg .= '<br>' . 'Form data: ' . var_export($_POST, 1) . '<br>';
+            if (empty($_POST['uname'])) {
+                $this->formErrors[] = 'Username can`t be empty!';
+            } else {
+                $this->uname = $_POST['uname'];
+            }
+            if (empty($_POST['umail'])) {
+                $this->formErrors[] = 'Email can`t be empty!';
+            } else {
+                $this->umail = $_POST['umail'];
+            }
+            if (!filter_var($_POST['umail'], FILTER_VALIDATE_EMAIL)) {
+                $this->formErrors[] = 'Email has wrong format!';
+            }
+            if (empty($_POST['umsg'])) {
+                $this->formErrors[] = 'Message can`t be empty!';
+            } else {
+                $this->umsg = $_POST['umsg'];
+            }
+            if (!empty($this->formErrors)) {
+                $this->formDataMsg .= 'Can`t add your comment because filled form has validation errors: ' . '<br>' . PHP_EOL;
+                foreach ($this->formErrors as $err) {
+                    $this->formDataMsg .= $err . '<br>' . PHP_EOL;
+                }
+            } else {
+                $this->commentData = $_POST;
+                $this->commentData['datetime'] = date('Y-m-d H:i:s');
+                $this->addComment($this->commentData);
+                header('Location: ' . $_SERVER['PHP_SELF'] . '?commentAdded' . time());
+            }
         }
     }
 
-    function addComment($aComm) {
-        global $commentsFile;
-        $allComments = getComments();
+    private function addComment($aComm) {
+        $allComments = $this->getComments();
         $allComments[] = $aComm;
         $sAllComm = serialize($allComments);
-        file_put_contents($commentsFile, $sAllComm);
+        file_put_contents($this->commentsFile, $sAllComm);
     }
 
-    function getComments() {
-        global $commentsFile;
+    private function getComments() {
         $aComm = [];
-        if (file_exists($commentsFile)) {
-            $sComm = file_get_contents($commentsFile);
+        if (file_exists($this->commentsFile)) {
+            $sComm = file_get_contents($this->commentsFile);
             $aComm = unserialize($sComm);
         }
         return $aComm;
     }
 
-    function filterBadWords($str) {
+    private function filterBadWords($str) {
         $badWords = ['fuck', 'sex', 'sheet'];
         $goodWords = ['f**k', 's*x', 'sh**t'];
         $str = str_ireplace($badWords, $goodWords, $str);
         return $str;
     }
 
-    $userComments = '';
-    $aComm = getComments();
-    if (!empty($aComm)) {
-        foreach ($aComm as $comm) {
-            if (!empty($comm)) {
-                $userComments .= '<dl>' .
-                    '<dl>Username: </dl><dd>' . $comm['uname'] . '</dd>' .
-                    '<dl>Email: </dl><dd>' . $comm['umail'] . '</dd>' .
-                    '<dl>Message: </dl><dd>' . filterBadWords($comm['umsg']) . '</dd>' .
-                    '<dl>Added on: </dl><dd>' . $comm['datetime'] . '</dd>' .
-                    '</dl><br>' . PHP_EOL;
-            }
-        }
+    private function setCommentsFileName()
+    {
+        $this->commentsFile = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'comments.dat';
     }
+}
+
+$contactForm = new ContactForm();
 
 ?>
 <!doctype html>
@@ -98,16 +155,16 @@
     <div>
         <h1>Contact</h1>
         <form action="" method="post">
-            <div><label for="uname">Username *: </label><input id="uname" type="text" name="uname" value="<?=$uname?>"></div>
-            <div><label for="umail">Email *: </label><input id="umail" type="email" name="umail" value="<?=$umail?>"></div>
-            <div><label for="umsg">Message *: </label><textarea id="umsg" name="umsg"><?=$umsg?></textarea></div>
+            <div><label for="uname">Username *: </label><input id="uname" type="text" name="uname" value="<?=$contactForm->getUname()?>"></div>
+            <div><label for="umail">Email *: </label><input id="umail" type="email" name="umail" value="<?=$contactForm->getUmail()?>"></div>
+            <div><label for="umsg">Message *: </label><textarea id="umsg" name="umsg"></textarea></div>
             <div><input type="submit" value="Add comment"></div>
         </form>
-        <div><?=$formDataMsg?></div>
+        <div><?=$contactForm->getFormDataMsg()?></div>
         <hr>
         <div>
             <h2>Added comments:</h2>
-            <div><?=$userComments?></div>
+            <div><?=$contactForm->displayComments()?></div>
         </div>
     </div>
 </body>
